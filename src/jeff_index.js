3fab73e1616349ef0e090a0c1a32e5d168bf2ab6
J2b2590 <J2b2590@gmail.com>
2020-11-09 16:32:08 -0600

    added form for new App and setting up html

--- src/jeff_index.js
+++ src/jeff_index.js
@@ -1,1 +1,15 @@
 console.log("TESTING JEFF")
+let newApp = false;
+
+const appAddBtn = document.querySelector("#new-app-id")
+const appFormContainer = document.querySelector(".new-app-form")
+appAddBtn.addEventListener("click", () =>{
+
+    newApp = !newApp;
+
+    if(newApp){
+        appFormContainer.style.display = "block";
+    } else {
+        appFormContainer.style.display = "none"
+    };
+});
