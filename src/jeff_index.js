8c7cdeac801193bb485a786333c30fcb9b392b75
J2b2590 <J2b2590@gmail.com>
2020-11-09 21:20:43 -0600

    working on flipping cards

--- src/jeff_index.js
+++ src/jeff_index.js
@@ -3,6 +3,10 @@ let newApp = false;
 
 const appAddBtn = document.querySelector("#new-app-id")
 const appFormContainer = document.querySelector(".new-app-form")
+
+const card = document.querySelector(".card")
+
+
 appAddBtn.addEventListener("click", () =>{
 
     newApp = !newApp;
@@ -11,5 +15,18 @@ appAddBtn.addEventListener("click", () =>{
         appFormContainer.style.display = "block";
     } else {
         appFormContainer.style.display = "none"
+    };
+});
+
+card.addEventListener("click", () =>{
+
+    console.log("click")
+
+    newApp = !newApp;
+
+    if(newApp){
+        card.classList.toggle("flipped")
+    } else {
+        console.log("help")
     };
 });
