eca2e6dc7ed8f72f1466304c8c6e94081db0f386
tincogs <tomoday91@gmail.com>
2020-11-11 11:40:09 -0600

    favorites display

--- src/tom_index.js
+++ src/tom_index.js
@@ -10,30 +10,87 @@ function ce(tag){
 
 const appAddBtn = document.querySelector("#new-app-id")
 const appFormContainer = document.querySelector(".new-app-form")
+const contFluid = document.getElementsByClassName("container-fluid")[0]
 
-const card = document.querySelector(".card")
 
 function main(){
     getApp()
-    fetchFavorites()
 }
 
 function getApp(){
     fetch("http://localhost:3000/appetizers")
       .then(resp => resp.json())
       .then(apps =>{
-        console.log(apps,"Call")
+        // console.log(apps,"Call")
         apps.forEach(app => renderApp(app))
     })
   }
 
   function renderApp(app){
     // console.log(app.image_src)
-    const imgTag = document.querySelector(".img-thumbnail")
+    const lastDiv = document.createElement('div')
+    lastDiv.className = "row"
+    const notLastDiv = document.createElement('div')
+    notLastDiv.className = "col"
+    const firstDiv = document.createElement('div')
+    firstDiv.className = "card shadow p-3 mb-5 bg-white rounded flex-fill front"
+    firstDiv.style.width = "18rem"
+    const imgTag = document.createElement("img")
+    imgTag.className = "img-thumbnail"
     imgTag.src = app.image_src
+    const secondDiv = document.createElement('div')
+    secondDiv.className = "card-body"
+    const thirdDiv = document.createElement('div')
+    thirdDiv.className = "card-face front"
+    const fourthDiv = document.createElement('div')
+    fourthDiv.className = "inside-info"
+    const fifthDiv = document.createElement('div')
+    fifthDiv.className = "arrows"
+    const h5 = document.createElement('h5')
+    h5.className = "card-title"
+    h5.innerText = `${app.title}`
+    const firstSvg = document.createElement("SVG")
+    firstSvg.style.width = "2em"
+    firstSvg.style.height = "2em"
+    firstSvg.className = "bi bi-caret-up"
+    firstSvg.viewBox = "0 0 16 16"
+    firstSvg.setAttribute("fill", "currentColor")
+    firstSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg")
 
-    const hFive = document.querySelector(".card-title")
-    hFive.innerHTML = app.title
+    const firstPath = document.createElement("path")
+    firstPath.setAttribute("d", "M3.204 11L8 5.519 12.796 11H3.204zm-.753-.659l4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659z")
+    firstPath.setAttribute("fill-rule", "evenodd")
+
+    const secondSvg = document.createElement("SVG")
+    secondSvg.style.width = "2em"
+    secondSvg.style.height = "2em"
+    secondSvg.className = "bi bi-caret-down"
+    secondSvg.viewBox = "0 0 16 16"
+    secondSvg.setAttribute("fill", "currentColor")
+    secondSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg")
+
+    const secondPath = document.createElement("path")
+    secondPath.setAttribute("d", "M3.204 5L8 10.481 12.796 5H3.204zm-.753.659l4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z")
+    secondPath.setAttribute("fill-rule", "evenodd")
+
+    const p = document.createElement("p")
+    p.innerText = `Likes: ${app.rating}`
+
+
+    firstDiv.appendChild(imgTag)
+    firstDiv.appendChild(secondDiv)
+    secondDiv.appendChild(thirdDiv)
+    thirdDiv.appendChild(fourthDiv)
+    fourthDiv.appendChild(h5)
+    fourthDiv.appendChild(fifthDiv)
+    fifthDiv.appendChild(firstSvg)
+    firstSvg.appendChild(firstPath)
+    fifthDiv.appendChild(secondSvg)
+    secondSvg.appendChild(secondPath)
+    fourthDiv.appendChild(p)
+    notLastDiv.appendChild(firstDiv)
+    lastDiv.appendChild(notLastDiv)
+    contFluid.appendChild(lastDiv)
 }
 
 
@@ -48,24 +105,11 @@ appAddBtn.addEventListener("click", () =>{
     };
 });
 
-card.addEventListener("click", () =>{
-
-    console.log("click")
-
-    newApp = !newApp;
-
-    if(newApp){
-        card.classList.toggle("flipped")
-    } else {
-        console.log("help")
-    };
-});
-
 const userFaveBtn = document.querySelector("#user-favs-id")
 const userFaves = document.querySelector(".user-favs")
 userFaveBtn.addEventListener("click", () =>{
     userFavorites = !userFavorites;
-
+    fetchFavorites()
     if(userFavorites){
         userFaves.style.display = "block";
     } else {
@@ -74,10 +118,28 @@ userFaveBtn.addEventListener("click", () =>{
 });
 
 function fetchFavorites () {
-    fetch(favoritesEndpoint)
-    .then(resp => resp.json)
-    .then(data => {console.log(data,"Call")
+    fetch("http://localhost:3000/appetizers")
+      .then(resp => resp.json())
+      .then(faves =>{
+        userFaves.innerHTML = ""
+        faves.forEach(fave => renderFavorite(fave))
     })
+}
+
+function renderFavorite (fave) {
+    if(fave.favorites.length > 0){
+        const faveCard = ce("div")
+        const faveImg = ce("img")
+        const faveTitle = ce("h6")
+
+        faveCard.setAttribute("class", "fave-card")
+        faveImg.setAttribute("class", "fave-image")
+        faveImg.src = fave.image_src
+        faveTitle.innerText = fave.title
+
+        faveCard.append(faveImg, faveTitle)
+        userFaves.append(faveCard)
+    }
 }
 
 main()
